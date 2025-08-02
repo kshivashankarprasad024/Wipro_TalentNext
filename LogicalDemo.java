d515e7aae2b721f6dab7ae303b3d9bee87bc5c1e
kshivashankarprasad024 <167082246+kshivashankarprasad024@users.noreply.github.com>
2025-08-02 13:09:07 +0530

    Create LogicalDemo.java

---
+++ LogicalDemo.java
@@ -0,0 +1,8 @@
+public class LogicalDemo {
+    public static void main(String[] args) {
+        int a = 10, b = 5, c = 20;
+        System.out.println((a < b) && (a < c)); // false
+        System.out.println((a < b) || (a < c)); // true
+        System.out.println(!(a < b)); // true
+    }
+}
