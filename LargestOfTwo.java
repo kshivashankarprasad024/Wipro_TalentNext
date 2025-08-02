5d5bcbddf3c67828825e875945d350a39b41e581
kshivashankarprasad024 <167082246+kshivashankarprasad024@users.noreply.github.com>
2025-08-02 13:10:09 +0530

    Create LargestOfTwo.java

---
+++ LargestOfTwo.java
@@ -0,0 +1,7 @@
+public class LargestOfTwo {
+    public static void main(String[] args) {
+        int a = 10, b = 20;
+        int max = (a > b) ? a : b;
+        System.out.println("Largest: " + max);
+    }
+}
