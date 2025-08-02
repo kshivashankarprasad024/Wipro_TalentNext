facf769c4c7621d583457ad9e05644f20f412fe1
kshivashankarprasad024 <167082246+kshivashankarprasad024@users.noreply.github.com>
2025-08-02 13:18:38 +0530

    Create LargestOfTwoIf.java

---
+++ LargestOfTwoIf.java
@@ -0,0 +1,9 @@
+public class LargestOfTwoIf {
+    public static void main(String[] args) {
+        int a = 12, b = 25;
+        if (a > b)
+            System.out.println("Largest is: " + a);
+        else
+            System.out.println("Largest is: " + b);
+    }
+}
