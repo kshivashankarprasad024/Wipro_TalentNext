f67ee679aabc78c1616389b4a19f2b171ddb2e3c
kshivashankarprasad024 <167082246+kshivashankarprasad024@users.noreply.github.com>
2025-08-02 13:16:18 +0530

    Create CheckPosNeg.java

---
+++ CheckPosNeg.java
@@ -0,0 +1,11 @@
+public class CheckPosNeg {
+    public static void main(String[] args) {
+        int num = -5;
+        if (num > 0)
+            System.out.println("Positive");
+        else if (num < 0)
+            System.out.println("Negative");
+        else
+            System.out.println("Zero");
+    }
+}
